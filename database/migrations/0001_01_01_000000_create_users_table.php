fe930e0015fb724c92f2305e7643cbf42fe83703
rpsohag <rpsohag.bd@gmail.com>
2024-03-23 21:36:53 +0600

    install breeze and add multi auth

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -16,6 +16,8 @@ return new class extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->string('avatar')->default('/uploads/avatar.png');
+            $table->enum('role',['user','admin'])->default('user');
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
