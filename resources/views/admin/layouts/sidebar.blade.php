c09db2e90dce9047273c193fbfeb5ccc0e43fe68
rpsohag <rpsohag.bd@gmail.com>
2024-03-29 23:48:38 +0600

    integrate admin template

---
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -0,0 +1,67 @@
+
+
+
+<nav class="navbar navbar-expand-lg main-navbar">
+    <form class="form-inline mr-auto">
+      <ul class="navbar-nav mr-3">
+        <li><a href="#" data-toggle="sidebar" class="nav-link nav-link-lg"><i class="fas fa-bars"></i></a></li>
+      </ul>
+     
+    </form>
+    <ul class="navbar-nav navbar-right">
+      <li class="dropdown"><a href="#" data-toggle="dropdown" class="nav-link dropdown-toggle nav-link-lg nav-link-user">
+        <img alt="image" src="assets/img/avatar/avatar-1.png" class="rounded-circle mr-1">
+        <div class="d-sm-none d-lg-inline-block">Hi, Ujang Maman</div></a>
+        <div class="dropdown-menu dropdown-menu-right">
+          <div class="dropdown-title">Logged in 5 min ago</div>
+          <a href="features-profile.html" class="dropdown-item has-icon">
+            <i class="far fa-user"></i> Profile
+          </a>
+          <a href="features-activities.html" class="dropdown-item has-icon">
+            <i class="fas fa-bolt"></i> Activities
+          </a>
+          <a href="features-settings.html" class="dropdown-item has-icon">
+            <i class="fas fa-cog"></i> Settings
+          </a>
+          <div class="dropdown-divider"></div>
+          <a href="#" class="dropdown-item has-icon text-danger">
+            <i class="fas fa-sign-out-alt"></i> Logout
+          </a>
+        </div>
+      </li>
+    </ul>
+  </nav>
+  <div class="main-sidebar sidebar-style-2">
+    <aside id="sidebar-wrapper">
+      <div class="sidebar-brand">
+        <a href="index.html">Stisla</a>
+      </div>
+      <div class="sidebar-brand sidebar-brand-sm">
+        <a href="index.html">St</a>
+      </div>
+      <ul class="sidebar-menu">
+        <li class="menu-header">Dashboard</li>
+        <li class="dropdown active">
+          <a href="#" class="nav-link" ><i class="fas fa-fire"></i> <span>Dashboard</span></a>
+         
+        </li>
+
+        <li class="menu-header">menu name</li>
+        <li class="dropdown">
+          <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-columns"></i> <span>Dropdown</span></a>
+          <ul class="dropdown-menu">
+            <li><a class="nav-link" href="">menu one</a></li>
+            <li><a class="nav-link" href="">menu two</a></li>
+            <li><a class="nav-link" href="">menu three</a></li>
+          </ul>
+        </li>
+       
+        
+      </ul>
+
+      <div class="mt-4 mb-4 p-3 hide-sidebar-mini">
+        <a href="https://getstisla.com/docs" class="btn btn-primary btn-lg btn-block btn-icon-split">
+          <i class="fas fa-rocket"></i> Documentation
+        </a>
+      </div>        </aside>
+  </div>
